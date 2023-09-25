5af9f15f41599d3e454a88a0722ea990dea272e2
admin <nguyennvph21987@gmail.com>
2023-09-25 20:43:33 +0700

    Start_all_friends

--- web_nodejs_cosplaysuit/app.js
+++ web_nodejs_cosplaysuit/app.js
@@ -4,10 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var categoryRouter = require('./routes/category');
+const { error } = require('console');
 
 var app = express();
 // view engine setup
@@ -19,29 +19,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://nguyenchamdut:lvC1Kanh5ygnYaHS@cluster0.chnbv9i.mongodb.net/App_cosplay_suit=true&w=majority";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- web_nodejs_cosplaysuit/models/db.model.js
+++ web_nodejs_cosplaysuit/models/db.model.js
@@ -1,23 +1,7 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://nguyenchamdut:lvC1Kanh5ygnYaHS@cluster0.chnbv9i.mongodb.net/App_cosplay_suit=true&w=majority";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://nguyenchamdut:lvC1Kanh5ygnYaHS@cluster0.chnbv9i.mongodb.net/?retryWrites=true&w=majority")
+.catch((err) => {
+    console.log("Lỗi kết nối csdl");
+    console.log(err);
 });
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+module.exports = {mongoose};
