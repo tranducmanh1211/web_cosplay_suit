554525852ba81367140a16101b75e213cec30798
manh <manhtdph19414@fpt.edu.vn>
2023-09-26 21:23:58 +0700

    Manh(26/09)

--- web_nodejs_cosplaysuit/routes/category.js
+++ web_nodejs_cosplaysuit/routes/category.js
@@ -1,7 +1,18 @@
 var express = require('express');
 var routes = express.Router();
-var tb_chuyentranhController = require('../controllers/API/Category.Api');
+var tb_productCtrl = require('../controllers/API/Category.Api');
+var tb_userCtrl = require('../controllers/API/users.api');
 
-routes.post('/addcategory', tb_chuyentranhController.AddCategory);
+
+
+
+routes.post('/addcategory', tb_productCtrl.AddCategory);
+
+
+
+
+routes.post('/regapp',tb_userCtrl.regApp);
+routes.post('/login',tb_userCtrl.login);
+
 
 module.exports = routes;
