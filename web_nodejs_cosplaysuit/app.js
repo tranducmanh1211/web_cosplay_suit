3e9f22df29d78d20569eca07c58a411723d197da
manh <manhtdph19414@fpt.edu.vn>
2023-09-30 17:07:39 +0700

    Manh(30/09)

--- web_nodejs_cosplaysuit/app.js
+++ web_nodejs_cosplaysuit/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var categoryRouter = require('./routes/category');
 var chatRouter = require('./routes/chat')
+var userapiRouter = require('./routes/user.api');
 const { error } = require('console');
 
 var app = express();
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/category', categoryRouter);
 app.use('/chat', chatRouter);
+app.use('/user/api', userapiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
