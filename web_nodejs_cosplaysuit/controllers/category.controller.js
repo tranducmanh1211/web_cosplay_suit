81da65d1fc9bc0b790b69ecf436ea6b71c211f77
manh <manhtdph19414@fpt.edu.vn>
2023-11-27 19:55:21 +0700

    Manh(27/11)

--- web_nodejs_cosplaysuit/controllers/category.controller.js
+++ web_nodejs_cosplaysuit/controllers/category.controller.js
@@ -4,13 +4,15 @@ const admin = require('firebase-admin');
 const serviceAccount = require('../serviceAccountKey.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  storageBucket: 'gs://duantotnghiepcosplaysuit.appspot.com',
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: 'gs://duantotnghiepcosplaysuit.appspot.com',
 });
 
 const bucket = admin.storage().bucket();
 const multer = require('multer');
 const upload = multer().single('imageCategory');
+const upload1 = multer().single('imageCategoryChuong');
+
 
 exports.gettheloai = async (req, res, next) => {
     let username = req.session.userU.fullname;
@@ -20,17 +22,17 @@ exports.gettheloai = async (req, res, next) => {
     let skip = (page - 1) * limit;
     if (typeof req.query.name !== 'undefined') {
         const keyword = req.query.name;
-        const regex = new RegExp('.*' + keyword + '.*', 'i'); 
+        const regex = new RegExp('.*' + keyword + '.*', 'i');
         dieu_kien_loc = { name: regex };
     }
-   
+
     try {
         var list = await myMD.tb_categoryModel.find(dieu_kien_loc)
-        let page_length = Math.ceil(list.length/limit);
+        let page_length = Math.ceil(list.length / limit);
 
         var list = await myMD.tb_categoryModel.find(dieu_kien_loc).skip(skip)
-        .limit(limit);;
-        res.render('navigation_view/quanlytheloai', { listTL: list, username: username,page_length : page_length,page : page });
+            .limit(limit);;
+        res.render('navigation_view/quanlytheloai', { listTL: list, username: username, page_length: page_length, page: page });
     } catch (error) {
         console.error(error);
         return res.status(500).send('Lỗi khi truy vấn dữ liệu từ MongoDB.');
@@ -68,7 +70,7 @@ exports.addTL = async (req, res, next) => {
                 });
 
                 stream.on('finish', async () => {
-                    
+
                     const [url] = await imageFile.getSignedUrl({ action: 'read', expires: '01-01-3000' });
 
                     var currentdate = new Date();
@@ -123,7 +125,7 @@ exports.delTL = async (req, res, next) => {
         console.log(`Deleting category with ID: ${idTL}`);
         const result = await myMD.tb_categoryModel.findByIdAndDelete(idTL, req.body);
         console.log(result);
-        
+
         const currentPage = req.query.page || 1;
         res.redirect(`/category/getCategory?page=${currentPage}`);
     } catch (error) {
@@ -134,58 +136,60 @@ exports.delTL = async (req, res, next) => {
 
 exports.editTL = async (req, res, next) => {
     let idTL = req.params.idTL;
+    upload1(req, res, async function (err) {
+        if (err) {
+            console.error(err);
+            return res.send('Lỗi khi xử lý dữ liệu form.');
+        }
+        try {
+            let objTL = await myMD.tb_categoryModel.findById(idTL);
+            console.log("manh: ",objTL.name);    
+            if (req.method === 'POST') {
+                const newName = req.body.name;
 
-    try {
-       
-        let objTL = await myMD.tb_categoryModel.findById(idTL);
+                // Nếu có file ảnh được chọn
+                if (req.file) {
+                    const file = req.file;
+                    console.log("manh:", file);
+                    const imageName = `${Date.now()}_${file.originalname}`;
+                    const imageFile = bucket.file(imageName);
+                    const stream = imageFile.createWriteStream({
+                        metadata: {
+                            contentType: file.mimetype,
+                        },
+                    });
 
-        if (req.method === 'POST') {
+                    stream.on('error', (error) => {
+                        console.error(error);
+                        return res.status(500).send('Lỗi khi tải lên ảnh.');
+                    });
 
-            const newName = req.body.name;
+                    stream.on('finish', async () => {
+                        const [url] = await imageFile.getSignedUrl({ action: 'read', expires: '01-01-3000' });
+                        objTL.name = newName;
+                        objTL.imageCategory = url;
+                        await objTL.save();
 
-            // Nếu có file ảnh được chọn
-            if (req.file) {
-                const file = req.file;
+                        const currentPage = req.query.page || 1;
+                        res.redirect(`/category/getCategory?page=${currentPage}`);
+                    });
 
-                const imageName = `${Date.now()}_${file.originalname}`;
-                const imageFile = bucket.file(imageName);
-                const stream = imageFile.createWriteStream({
-                    metadata: {
-                        contentType: file.mimetype,
-                    },
-                });
-
-                stream.on('error', (error) => {
-                    console.error(error);
-                    return res.status(500).send('Lỗi khi tải lên ảnh.');
-                });
-
-                stream.on('finish', async () => {
-                    const [url] = await imageFile.getSignedUrl({ action: 'read', expires: '01-01-3000' });
+                    stream.end(file.buffer);
+                } else {
+                    // Nếu không có file ảnh, chỉ cập nhật tên
                     objTL.name = newName;
-                    objTL.imageCategory = url;
                     await objTL.save();
 
                     const currentPage = req.query.page || 1;
                     res.redirect(`/category/getCategory?page=${currentPage}`);
-                    return;
-                });
-
-                stream.end(file.buffer);
+                }
             } else {
-                // Nếu không có file ảnh, chỉ cập nhật tên
-                objTL.name = newName;
-                await objTL.save();
-            
-                const currentPage = req.query.page || 1;
-                res.redirect(`/category/getCategory?page=${currentPage}`);
+                // Nếu không phải là phương thức POST, chỉ render template
+                res.render('navigation_view/quanlytheloai', { objTL: objTL });
             }
+        } catch (error) {
+            console.error(error);
+            return res.status(500).send('Lỗi khi xử lý dữ liệu.');
         }
-
-      
-        res.render('navigation_view/quanlytheloai', { objTL: objTL });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).send('Lỗi khi xử lý dữ liệu.');
-    }
+    });
 };
