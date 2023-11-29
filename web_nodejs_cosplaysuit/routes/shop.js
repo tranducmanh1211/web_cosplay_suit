1f4fb189254df9eaa9d9769d31073954f91dd0ec
manh <manhtdph19414@fpt.edu.vn>
2023-11-29 13:32:26 +0700

    Manh(29/11)

--- web_nodejs_cosplaysuit/routes/shop.js
+++ web_nodejs_cosplaysuit/routes/shop.js
@@ -6,6 +6,6 @@ var tb_shop = require('../controllers/API/Shop.Api');
 router.get('/listShop/:id_user', tb_shop.getlListShop);
 router.get('/getOneShop/:id', tb_shop.getShopById);
 
-
+router.get('/getShopById/:id',tb_shop.getShopById1)
 
 module.exports = router;
