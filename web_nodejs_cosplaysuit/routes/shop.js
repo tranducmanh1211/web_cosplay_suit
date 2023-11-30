84e4d9d36d4912ec2cbba673061ffc1401b7b714
test <binhdtph26505@fpt.edu.vn>
2023-12-01 00:18:11 +0700

    ctiet shop

--- web_nodejs_cosplaysuit/routes/shop.js
+++ web_nodejs_cosplaysuit/routes/shop.js
@@ -1,20 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var tb_shop = require('../controllers/API/Shop.Api');
-var shopController = require('../controllers/shop.controller');
-var check_login = require('../middlewares/check_login');
-router.use((req, res, next) => {
-    console.log('middleware');
-    next();
-});
+
+
 //asdasdas
 
-router.get('/listShop/:id_user',check_login.yeu_cau_login, tb_shop.getlListShop);
-router.get('/getOneShop/:id',check_login.yeu_cau_login, tb_shop.getShopById);
+router.get('/listShop/:id_user', tb_shop.getlListShop);
+router.get('/getOneShop/:id', tb_shop.getShopById);
 
-router.get('/getShopById/:id',check_login.yeu_cau_login,tb_shop.getShopById1)
-router.get('/listShop',check_login.yeu_cau_login, tb_shop.getlListShop1);
+router.get('/getShopById/:id',tb_shop.getShopById1)
+router.get('/listShop', tb_shop.getlListShop1);
 
-router.get('/getCT/:id_shop',check_login.yeu_cau_login, shopController.getShopProducts);
 
 module.exports = router;
