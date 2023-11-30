f9e8cac9d506efdb6b17ccb270b4fef0111dd7ca
manh <manhtdph19414@fpt.edu.vn>
2023-11-30 20:19:10 +0700

    Manh(30/11)

--- web_nodejs_cosplaysuit/routes/product.api.js
+++ web_nodejs_cosplaysuit/routes/product.api.js
@@ -6,6 +6,7 @@ var tb_CategoryCtrl = require('../controllers/API/Category.Api');
 routes.get('/getlistsp', tb_productCtrl.getlListSanPham);
 routes.get('/getlistsp/:id_shop', tb_productCtrl.getproductUser);
 routes.get('/getlistsplimit/:id_shop', tb_productCtrl.getproductByIdShop);
+routes.get('/getlistsp/:id_shop/:page', tb_productCtrl.getproductByIdShopPage);
 routes.put('/updateSP/:id', tb_productCtrl.updateProduct);
 routes.delete('/delSP/:id', tb_productCtrl.delProduct);
 routes.post('/addSP', tb_productCtrl.AddProduct);
