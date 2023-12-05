a0230e06fa7de3a485bbca905cd79d0e046bbac1
test <binhdtph26505@fpt.edu.vn>
2023-12-05 19:21:36 +0700

    tke shop

---
+++ web_nodejs_cosplaysuit/controllers/API/ThongKe.API.js
@@ -0,0 +1,13 @@
+var myMD = require('../../models/Bill.model');
+
+
+exports.getBillTke = async (req, res, next) => {
+    //Lấy ds đơn hàng theo id_user
+    let dieu_kien_loc = null;
+    if (typeof (req.params.id_shop) != 'undefined') {
+        dieu_kien_loc = { id_shop: req.params.id_shop};
+    }
+    var list = await myMD.tb_billModel.find({$and: [dieu_kien_loc , {status: 'Done'}]}).populate('id_shop').populate('id_user');
+
+    res.send(list);
+}
