3bd283f349c9e475274a9661bd0f4acdc826f6e6
manh <manhtdph19414@fpt.edu.vn>
2023-12-06 16:52:15 +0700

    Manh(06/12)

--- web_nodejs_cosplaysuit/controllers/API/Voucher.api.js
+++ web_nodejs_cosplaysuit/controllers/API/Voucher.api.js
@@ -1,6 +1,7 @@
 var myMDDD = require('../../models/cosplay_suit_model');
 var myDBshop = require('../../models/cosplau_suit_user_model');
 var myMD = require('../../models/Bill.model');
+const { ObjectId } = require('mongoose').Types;
 var objReturn = {
     stu: 1,
     msg: 'ok'
@@ -9,12 +10,12 @@ var objReturn = {
 
 exports.getListVoucher = async (req, res, next) => {
     let dieu_kien_loc = null;
-  
+
 
     if (typeof req.params.id_shop !== 'undefined') {
         dieu_kien_loc = { id_shop: req.params.id_shop };
     }
-    var list = await  myMDDD.tb_voucherModel.find({id_shop: req.params.id});
+    var list = await myMDDD.tb_voucherModel.find({ id_shop: req.params.id });
 
     res.send(list);
 }
@@ -74,26 +75,57 @@ exports.delVoucher = async (req, res, next) => {
 
 }
 
-exports.listUserByShop = async ( req,res,next) =>{
-    const idshop = await myDBshop.tb_shopModel.findOne({id_user: req.params.id}).select('_id');
-    
+exports.listUserByShop = async (req, res, next) => {
+    const idshop = await myDBshop.tb_shopModel.findOne({ id_user: req.params.id }).select('_id');
+
     const cartlist = await myMD.tb_billModel.find({ id_shop: idshop }).select('id_user').lean();
 
     const idshoplist = new Set(cartlist.map(hd => String(hd.id_user)));
     const giaTriKhongTrungLap = [...idshoplist];
 
     //Lấy ds user từ ds không trùng
-    const list = await myDBshop.tb_profileModel.find({id_user: { $in: giaTriKhongTrungLap }}).populate('id_user');
+    const list = await myDBshop.tb_profileModel.find({ id_user: { $in: giaTriKhongTrungLap } }).populate('id_user');
 
     res.send(list);
 }
 
-exports.seenVoucher = async ( req, res ,next) =>{
-    let seenVoucers = new myMDDD.tb_seenvoucher();
-    seenVoucers.id_user = req.body.id_user;
-    seenVoucers.id_voucher = req.body.id_voucher;
-    let new_CMD = await seenVoucers.save();
-    console.log(new_CMD);
-   
-    res.json(new_CMD);
+exports.seenVoucher = async (req, res, next) => {
+    let msg = '';
+    let dieu_kien_loc = null;
+    try {
+        const existingVoucher = await myMDDD.tb_seenvoucher.findOne({id_user : req.body.id_user});
+
+        if (!existingVoucher) {
+            const seenVoucers = new myMDDD.tb_seenvoucher();
+            seenVoucers.id_user = req.body.id_user;
+            seenVoucers.id_voucher = req.body.id_voucher;
+    
+            let new_CMD = await seenVoucers.save();
+            return res.status(201).json({ seenVoucher: new_CMD, msg: "Gửi voucher thành công!" });
+        }
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ msg: "Đã xảy ra lỗi!" });
+    }
+}
+exports.listSeenByIdVoucher = async ( req, res,next)=>{
+    const list = await myMDDD.tb_seenvoucher.find({id_voucher : req.params.id_voucher});
+
+    res.json(list);
+}
+exports.updateVoucherSeen = async (req, res, next) => {
+
+    try {
+
+        const addCM = await myMDDD.tb_voucherModel.findById(req.params.id);
+        addCM.amount = req.body.amount;
+
+        const mtSave = await addCM.save();
+        res.json(mtSave);
+    } catch (error) {
+        res.send(error)
+        console.log(error);
+
+    }
 }
--- web_nodejs_cosplaysuit/routes/voucher.js
+++ web_nodejs_cosplaysuit/routes/voucher.js
@@ -9,4 +9,6 @@ routes.post('/postvoucher',tb_VcCtrl.AddVoucher);
 routes.delete('/delvoucher/:id',tb_VcCtrl.delVoucher);
 routes.get('/getdskhachvoucher/:id',tb_VcCtrl.listUserByShop);
 routes.post('/seenvoucher',tb_VcCtrl.seenVoucher);
+routes.put('/updatevoucherSeen/:id',tb_VcCtrl.updateVoucherSeen);
+routes.get('/listvoucher/:id_voucher',tb_VcCtrl.listSeenByIdVoucher);
 module.exports = routes;
